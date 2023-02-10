3ec75bef5d4ab5d73b6b4c112d031b6776b0a3f5
gaetanRgd <regaud.gaetan@gmail.com>
2023-02-10 16:20:28 +0100

    reverse commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,15 +2,6 @@ package src.main;
 
 public class Main {
 	public static void main(String arg[]){
-		Graph g = TestGraph.test4(7,10,0);
-		if(!Algorithm.backtrack(g,true)){
-			System.out.println("**************");
-			System.out.println("SOS");
-		}
-		else{
-			System.out.println("***solution***");
-			Graph.pp(g);
-		}
-		System.out.println("**************");
+	
 	}
 }
