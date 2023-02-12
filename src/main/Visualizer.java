756df7e91c58b0204b95a42318d1cc824e2d51e8
Chanattan <zentsugo@gmail.com>
2023-02-12 02:47:18 +0100

    Promised jar version 1.0.1.

--- src/main/Visualizer.java
+++ src/main/Visualizer.java
@@ -4,32 +4,31 @@ import java.awt.event.KeyListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
-public class Visualizer implements KeyListener {
+public class Visualizer extends JFrame implements KeyListener {
 
-	private JFrame frame = null;
 	private RikudoPane pane;
 	public final static String prefix = "[Visualizer] ";
 	public int global_x[], global_y[]; //translations depend on the mode
 	public float zoom[]; //zoom too
 	
-    public Visualizer(Graph graph) { //Should be Rikudo rikudo
+    public Visualizer(Graph graph) {
+    	setIconImage(new ImageIcon(getClass().getResource("/jgc.jpg")).getImage()); //Should be Rikudo rikudo
     	if (graph == null) {
     		System.err.println(prefix + "Error, tried to visualize a null graph.");
     		System.exit(1);
     	}
-        this.frame = new JFrame();
-        this.frame.setTitle("Rikudo Creator JGC v1.0");
+        this.setTitle("Rikudo Creator JGC v1.0");
         GraphV g = new GraphV(graph);
-        pane = new RikudoPane(frame, this, g);
+        pane = new RikudoPane(this, this, g);
         this.global_x = new int[] {0,0};
         this.global_y = new int[] {0,0};
         this.zoom = new float[] {1f,1f};
-        frame.setContentPane(pane);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.addWindowListener(new WindowListener() {
+        setContentPane(pane);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        addWindowListener(new WindowListener() {
 			@Override
 			public void windowOpened(WindowEvent e) {
 			}
@@ -53,25 +52,19 @@ public class Visualizer implements KeyListener {
 			public void windowDeactivated(WindowEvent e) {
 			}
         });
-        frame.pack();
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
-        frame.addKeyListener(this);
+        pack();
+        setResizable(false);
+        setLocationRelativeTo(null);
+        addKeyListener(this);
     }
     
-    public void show() {
-    	if (this.frame != null) {
-    		this.frame.setVisible(true);
-    	}
-    }
-
 	@Override
 	public void keyTyped(KeyEvent e) {
 		if (e.getKeyChar() == 'd' || e.getKeyChar() == 'D') RikudoPane.DEBUG_MODE = !RikudoPane.DEBUG_MODE;
 		if (e.getKeyChar() == 'm' || e.getKeyChar() == 'M') {
 			RikudoPane.MODE = (RikudoPane.MODE + 1) % 2;
 			if (RikudoPane.MODE == 1) {
-				CreatorDialog cd = new CreatorDialog(frame, pane);
+				CreatorDialog cd = new CreatorDialog(this, pane);
 			}
 		}
 	}
